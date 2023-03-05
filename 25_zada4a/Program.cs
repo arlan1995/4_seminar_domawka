3a630adfd047a51a35923eb6eac8306db6c5583a
Arlan <earl199529@gmail.com>
2023-03-05 16:25:32 +0600

    4 семинар домашка

---
+++ 25_zada4a/Program.cs
@@ -0,0 +1,27 @@
+﻿//  Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Math.Pow использовать нельзя
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+class Program
+{
+    public static int Main()
+    {
+        //число, которое нужно возвести в степень
+        Console.Write("Введите a: ");
+        int a = int.Parse(Console.ReadLine());
+        //степень числа
+        Console.Write("Введите b: ");
+        int b = int.Parse(Console.ReadLine());
+        //число, возведенное в степень
+        int a_b = 1;
+        for (int i = 0; i < b; i++)
+        {
+            a_b *= a;
+        }
+        Console.WriteLine("{0} ^ {1} = {2}", a, b, a_b);
+        Console.ReadKey();
+        return 0;
+    }
+}
---
+++ 29_zada4a/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите метод, который задаёт массив из N элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+class Program
+{
+    static void Main(string[] array)
+    {
+        Console.Write("Введите длину массива: ");
+        int count = int.Parse(Console.ReadLine());
+        int[] myArray = new int[count];
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            Console.Write($"Введите элемент массива под индексом {i}: ");
+            myArray[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Вывод массива: ");
+        System.Console.Write("[");
+        for (int i = 0; i < myArray.Length - 1; i++)
+        {
+            Console.Write($"{myArray[i]}, ");
+        }
+        System.Console.Write($"{myArray[myArray.Length - 1]}");
+        System.Console.Write("]");
+    }
+}
